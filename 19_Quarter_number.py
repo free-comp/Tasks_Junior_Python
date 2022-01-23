f9db075b040d9c18cdf8f0dd9f33823a09b90659
AlisBrk <free-comp@yandex.ru>
2022-01-23 20:26:14 +0300

    Исправлены возвращаемые значения (более универсальная функция)

--- 19_Quarter_number.py
+++ 19_Quarter_number.py
@@ -3,13 +3,13 @@
 
 def quart(x,y):
     if (x > 0 and y > 0):
-        return '1 четверти'
+        return 1
     elif (x > 0 and y < 0):
-        return '2 четверти'
+        return 2
     elif (x < 0 and y < 0):
-        return '3 четверти'
+        return 3
     else:
-        return '4 четверти'
+        return 4
 
 import random
 
@@ -19,5 +19,5 @@ y = random.randint(-10,10)
 if (x == 0 or y == 0):
     print (f'Координаты точек x = {x}, y = {y}, номер четверти определить не получится. Попробуйте снова')
     exit()
-print (f'Координаты точек x = {x}, y = {y}, что соответствует {(quart(x,y))} системы координат')
+print (f'Координаты точек x = {x}, y = {y}, что соответствует {(quart(x,y))} четверти системы координат')
 
