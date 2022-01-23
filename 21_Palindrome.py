ae292341c989ca18bbd876cf7403ff3a20657736
AlisBrk <free-comp@yandex.ru>
2022-01-23 23:37:57 +0300

    Упрощено вычисление в функции

--- 21_Palindrome.py
+++ 21_Palindrome.py
@@ -3,9 +3,9 @@
 def pldr(number):
     number = abs(number) # на случай ввода отрицательного числа
     first = number % 10
-    second = (number % 100 - number % 10)//10
-    fourth = (number % 10000 - number % 1000)//1000
-    fifth = (number % 100000 - number % 10000)//10000
+    second = number % 100 // 10
+    fourth = number % 10000 // 1000
+    fifth = number % 100000 // 10000
 
     if (first == fifth and second == fourth):
         return True
